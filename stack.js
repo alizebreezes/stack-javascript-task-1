c7609d7770a20068a48fb86395559a9573473f5e
Alize Lee <alizebreezes@gmail.com>
2021-04-02 23:34:38 +0200

    Alize Lee Solution

--- stack.js
+++ stack.js
@@ -1,1 +1,59 @@
 // Your implementation goes here
+module.exports = class Stack {
+  constructor(...elements) {
+    this.elements = elements;
+    this.size = this.elements.length;
+    this.top = this.elements[this.size - 1];
+  }
+
+  // Adds new element
+  push(newElement) {
+    this.elements.push(newElement);
+    this.size = this.elements.length;
+  }
+
+  // Checks if elements is empty
+  isEmpty() {
+    return this.elements.length == 0;
+  }
+
+  // Removes last element
+  pop() {
+    if (this.elements.length == 0) {
+      return null;
+    } else {
+      return this.elements.pop();
+    }
+  }
+
+  // Returns last element
+  peek() {
+    return this.top;
+  }
+
+  // Swaps last two elements positions
+  swap() {
+    if (this.elements.length == 0) {
+      return null;
+    } else {
+      let top = this.elements[this.size - 1];
+      let newTop = this.elements[this.size - 2];
+
+      this.elements[this.size - 1] = this.elements[this.size - 2];
+      this.elements[this.size - 2] = top;
+      this.top = newTop;
+
+      console.log(top);
+      console.log(this.elements);
+      console.log(this.top);
+
+      return this.top;
+    }
+
+    // var temp = this.elements[this.elements.length - 1];
+    // this.elements[this.elements.length - 1] = this.elements[
+    //   this.elements.length - 2
+    // ];
+    // this.elements[this.elements.length - 2] = temp;
+  }
+};
--- stack.test.js
+++ stack.test.js
@@ -1,55 +1,55 @@
-const Stack = require('./stack')
+const Stack = require("./stack");
 
-describe('Stack', () => {
-  it('stack size', () => {
-    expect(new Stack().size).toBe(0)
-    expect(new Stack(1, 2).size).toBe(2)
-    expect(new Stack(1, 2, 3).size).toBe(3)
-  })
+describe("Stack", () => {
+  it("stack size", () => {
+    expect(new Stack().size).toBe(0);
+    expect(new Stack(1, 2).size).toBe(2);
+    expect(new Stack(1, 2, 3).size).toBe(3);
+  });
 
-  it('create stack with initial values', () => {
-    const stack = new Stack('abc', 123, false)
-    expect(stack.size).toBe(3)
-  })
+  it("create stack with initial values", () => {
+    const stack = new Stack("abc", 123, false);
+    expect(stack.size).toBe(3);
+  });
 
-  it('add element to stack', () => {
-    const stack = new Stack()
+  it("add element to stack", () => {
+    const stack = new Stack();
 
-    stack.push('first')
-    stack.push('second')
+    stack.push("first");
+    stack.push("second");
 
-    expect(stack.size).toBe(2)
-  })
+    expect(stack.size).toBe(2);
+  });
 
-  it('isEmpty', () => {
-    expect(new Stack().isEmpty()).toBe(true)
-    expect(new Stack(1, 2, 3).isEmpty()).toBe(false)
-  })
+  it("isEmpty", () => {
+    expect(new Stack().isEmpty()).toBe(true);
+    expect(new Stack(1, 2, 3).isEmpty()).toBe(false);
+  });
 
-  it( 'pop value from stack', () => {
-    const stack = new Stack('first', 'second', 'third')
+  it("pop value from stack", () => {
+    const stack = new Stack("first", "second", "third");
 
-    expect(stack.pop()).toBe('third')
-    expect(stack.pop()).toBe('second')
-    expect(stack.pop()).toBe('first')
-    expect(stack.pop()).toBe(null)
-  })
+    expect(stack.pop()).toBe("third");
+    expect(stack.pop()).toBe("second");
+    expect(stack.pop()).toBe("first");
+    expect(stack.pop()).toBe(null);
+  });
 
-  it('peek value from stack', () => {
-    const stack = new Stack(1, 2, 3)
+  it("peek value from stack", () => {
+    const stack = new Stack(1, 2, 3);
 
-    expect(stack.peek()).toBe(3)
-    expect(stack.peek()).toBe(3)
-  })
+    expect(stack.peek()).toBe(3);
+    expect(stack.peek()).toBe(3);
+  });
 
-  it('swap stack elements', () => {
-    const stack = new Stack(1, 2, 3)
+  it("swap stack elements", () => {
+    const stack = new Stack(1, 2, 3);
 
-    stack.swap()
+    stack.swap();
 
-    expect(stack.peek()).toBe(2)
-    expect(stack.pop()).toBe(2)
-    expect(stack.pop()).toBe(3)
-    expect(stack.pop()).toBe(1)
-  })
-})
+    expect(stack.peek()).toBe(2);
+    expect(stack.pop()).toBe(2);
+    expect(stack.pop()).toBe(3);
+    expect(stack.pop()).toBe(1);
+  });
+});
